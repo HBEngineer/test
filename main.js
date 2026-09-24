49725a4f4f37288d7902822a9d1dc08b1c4a851b
HBEngineer <52140316+HBEngineer@users.noreply.github.com>
2026-09-24 10:46:56 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -276,9 +276,9 @@ if (ctrlAngle) {
 const AXIS_CONFIG = {
   PosA1: { nodeName: 'Degree1', axis: 'y', sign:  1, offset: 0, valueElementId: 'val-a1' },
   PosA2: { nodeName: 'Degree2', axis: 'z', sign:  1, offset: 0, valueElementId: 'val-a2' },
-  PosA3: { nodeName: 'Degree3', axis: 'z', sign: -1, offset: 0, valueElementId: 'val-a3' },
+  PosA3: { nodeName: 'Degree3', axis: 'z', sign:  1, offset: 0, valueElementId: 'val-a3' },
   PosA4: { nodeName: 'Degree4', axis: 'x', sign:  1, offset: 0, valueElementId: 'val-a4' },
-  PosA5: { nodeName: 'Degree5', axis: 'x', sign:  1, offset: 0, valueElementId: 'val-a5' },
+  PosA5: { nodeName: 'Degree5', axis: 'y', sign:  1, offset: 0, valueElementId: 'val-a5' },
   PosA6: { nodeName: 'Degree6', axis: 'y', sign:  1, offset: 0, valueElementId: 'val-a6' }
   // Add/remove entries here to change the DOF count. Every entry added here
   // needs a matching HTML telemetry row (index.html) and, if you want the
